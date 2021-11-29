086de2a98fd115c5c944400183e3d2c98c72c39e
Mendelsohn Chan <chan.mendelsohn.neil@gmail.com>
2021-11-29 23:25:42 +0800

    Commit on Nov 29, 2021

---
+++ W01_CAMEL_CASE.py
@@ -0,0 +1,50 @@
+import re
+while True:
+    try:
+        s = input().rstrip()    
+        sc, mcv, op = s.split(";")
+        if sc == "S":
+            if mcv == "M":
+                cap = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', op[:-2])
+                ans = " ".join(m.group(0).lower() for m in cap)
+                print(ans.rstrip())
+                
+            if mcv == "C":
+                cap = re.findall(r'[A-Z](?:[a-z]+|[A-Z]*(?=[A-Z]|$))', op)
+                ans = " ".join(i.lower() for i in cap)
+                print(ans.rstrip())
+                
+            if mcv == "V":
+                cap = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', op)
+                ans = " ".join(m.group(0).lower() for m in cap)
+                print(ans.rstrip())
+                
+        if sc == "C":
+            if mcv == "M":
+                lst = op.split(" ")
+                res = []
+                for i in range(len(lst)):
+                    if i == 0:
+                        res.append(lst[i].lower())
+                    else:
+                        res.append(lst[i].capitalize())
+                ans = "".join(i for i in res)
+                ans = ans.rstrip() + "()"
+                print(ans.rstrip())
+            if mcv == "C":
+                lst = op.split(" ")
+                ans = "".join(i.capitalize() for i in lst)
+                print(ans.rstrip())
+            if mcv == "V":
+                lst = op.split(" ")
+                res = []
+                for i in range(len(lst)):
+                    if i == 0:
+                        res.append(lst[i].lower())
+                    else:
+                        res.append(lst[i].capitalize())
+                ans = "".join(i for i in res)
+                print(ans.rstrip())
+            
+    except EOFError:
+        break
